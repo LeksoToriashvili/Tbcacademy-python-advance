c344f1eeb3ad799a2ab47d74efd08f39d9a4e732
LeksoToriashvili <ltoriashvili@gmail.com>
2024-09-16 19:10:17 +0400

    add timer to homework3

--- homework_3/main.py
+++ homework_3/main.py
@@ -1,5 +1,6 @@
 import requests
 import threading
+import time
 
 
 BASE_URL = "https://jsonplaceholder.typicode.com/posts/"
@@ -11,25 +12,32 @@ def fetch_data(url, lock):
     if response.status_code == 200:
         with lock:
             with open('data.txt', 'a') as file:
-                file.write(response.text)
-                file.write('\n')
-    else:
-        with lock:
-            with open('data.txt', 'a') as file:
-                file.write(f"Cant fetch data for url: {url}")
-                file.write('\n')
+                if threading.active_count() == 2:
+                    file.write(response.text)
+                    file.write('\n]\n')
+                else:
+                    file.write(response.text)
+                    file.write(',\n')
 
 
 def main():
     file = open("data.txt", "w")
+    file.write("[\n")
     file.close()
 
     lock = threading.Lock()
+    threads = list()
 
     for i in range(1, 78):
         thread = threading.Thread(target=fetch_data, args=(BASE_URL + str(i), lock))
         thread.start()
+        threads.append(thread)
+
+    for thread in threads:
+        thread.join()
 
 
 if __name__ == '__main__':
+    t = time.time()
     main()
+    print(f"elapsed time: {time.time() - t}")
